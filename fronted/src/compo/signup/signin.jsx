60e93a0c7b650e9ff0a8fb65a52f92d873317c1a
rahullm9 <rahulmahato10101@gmail.com>
2025-03-09 19:58:20 +0530

    todo page updated

--- fronted/src/compo/signup/signin.jsx
+++ fronted/src/compo/signup/signin.jsx
@@ -12,10 +12,10 @@ const signin = () => {
 
           <div className="col-lg-8 column d-flex justify-content-center align-items-center">
             <div className="signup-form d-flex flex-column w-100 p-5 ">
-              <input type="text" className="username" placeholder='Enter username' />
-              <input type="email" className="email" placeholder='Enter Email' />
+              <input type="text" className="username" placeholder='Enter username or Email' />
+              
               <input type="password" className="password" placeholder='Enter password' />
-              <button className="btn-signup p-2 ">Sign Up</button>
+              <button className="btn-signup p-2 ">Sign In</button>
             </div>
           </div>
 
--- fronted/src/compo/todo/Todo.jsx
+++ fronted/src/compo/todo/Todo.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import "./Todo.css"
+import React, { useState } from "react";
+import "./Todo.css";
+import "../home/Home.css"
 
 const Todo = () => {
-  return (
-    <div className='todo container '>
-      todo
-    </div>
-  )
-}
+  const [showTextarea, setShowTextarea] = useState(false);
 
-export default Todo
+  const show = () => {
+    setShowTextarea(true);
+  };
+
+  const change = () => {
+
+  }
+
+  return (
+    <div className="todo container">
+      <div className="todo-main d-flex flex-column justify-content-center align-items-center my-4">
+        <div className="d-flex flex-column w-50 todo-input-div p-3">
+          <input
+            className="todo-inputs my-2 p-2"
+            type="text"
+            placeholder="Title"
+            onClick={show}
+            onChange={change}
+            name="title"
+          />
+          {showTextarea && (
+            <textarea
+              name="body"
+              className="p-2 todo-inputs"
+              id="textarea"
+              placeholder="Body"
+              onChange={change}
+            />
+          )}
+        </div>
+
+        <div className="w-50 d-flex justify-content-end my-2">
+          <button className="home-btn p-2">add</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
